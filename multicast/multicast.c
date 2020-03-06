5beef5a3b6b9a5aef521c16b8238ae5b2442daee
Alex Bienstock <afb2136@columbia.edu>
2020-03-06 16:01:58 -0500

    multicast ops, current users ll, counts array for enc, etc

--- multicast/multicast.c
+++ multicast/multicast.c
@@ -1,2 +1,25 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "../trees/trees.h"
+
+//void *multicast_init(int tree_type, void *tree_flags, int n, int distrib, float add_wt, )
+
+struct Node *mult_add(struct Multicast *multicast, void *data) {
+  struct Node *added;
+  if (multicast->tree_type == 0)
+    added = gen_tree_add(multicast->tree, data, &lbbt_add);
+  else
+    added = gen_tree_add(multicast->tree, data, &btree_add);
+  return added;
+}
+
+void mult_update(struct Multicast *multicast, struct Node *user) { //TODO: user should be node???
+  gen_tree_upd(multicast->tree, user);
+}
+
+void mult_rem(struct Muticast *multicast, struct Node *user) { //TODO: user should be node??
+    if (multicast->tree_type == 0)
+      gen_tree_rem(multicast->tree, user, &lbbt_rem);
+    else
+      gen_tree_rem(multicast->tree, user, &btree_rem);
+}
--- multicast/multicast.h
+++ multicast/multicast.h
@@ -1,5 +1,15 @@
+#include "../trees/ll.h"
+
 struct Multicast {
-	int epoch;
-	int *users;
-	void *tree;
+  int epoch;
+  struct List *users; // ll for now... should be hashmap maybe??? users in group at certain time (most recently added in front)
+  void *tree;
+  int *counts;
+  int tree_type; //0 for lbbt, 1 for btree
 };
+
+struct Node *mult_add(struct Multicast *multicast, void *data); //TODO: data is for new user
+
+void mult_update(struct Multicast *mluticast, struct Node *user);
+
+void mult_rem(struct Muticast *multicast, struct Node *user);
