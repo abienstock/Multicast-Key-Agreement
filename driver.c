430b1d0b74eb5681577a94e38ff45b0372bcf348
Alexander Bienstock <afb383@access1.cims.nyu.edu>
2020-03-06 18:12:30 -0500

    fix max_id problems, random seed, freeing in driver

--- driver.c
+++ driver.c
@@ -1,9 +1,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
+#include <time.h>
 #include "trees/trees.h"
 #include "trees/ll.h"
 #include "multicast/multicast.h"
+
+/*static void printIntLine(void *p)
+{
+  struct Node *node = (struct Node *) p;
+  if (node->data == NULL)
+    printf("blank");
+  else
+    printf("%d", *(int *)node->data);
+    }*/
 
 int rand_int(int n, int distrib, float geo_param) {
   int i;
@@ -17,7 +27,7 @@ int rand_int(int n, int distrib, float geo_param) {
     }
     return n-1; // 0-indexed
   default:
-    return NULL; //TODO: error checking!!
+    return -1; //TODO: error checking!!
   }
 }
 
@@ -25,19 +35,33 @@ int next_op(struct Multicast *multicast, float add_wt, float upd_wt, int distrib
   int num_users = multicast->users->len;
   float operation = (float) rand() / (float) RAND_MAX;
   if (operation < add_wt || num_users == 1) { //TODO: forcing add with n=1 correct??
-    *max_id++; //so adding new users w.l.o.g.
-    struct Node *added = mult_add(multicast, (void *) max_id);
+    (*max_id)++; //so adding new users w.l.o.g.
+    printf("add: %d\n", *max_id);
+    int *newdata = malloc(sizeof(int));
+    if (newdata == NULL) {
+      perror("malloc returned NULL");
+      return -1; // TODO: error checking!!
+    }
+    *newdata = *max_id;
+    struct Node *added = mult_add(multicast, (void *) newdata);
     addAfter(multicast->users, multicast->users->tail, added);
+    //pretty_traverse_tree(((struct LBBT *)multicast->tree)->root, 0, &printIntLine);
     return 0;
   } else if (operation < add_wt + upd_wt) {
+    printf("upd\n");
     int user = rand_int(num_users, distrib, geo_param);
     struct Node *user_node = (struct Node *) findNode(multicast->users, user)->data;
     mult_update(multicast, user_node);
+    //pretty_traverse_tree(((struct LBBT *)multicast->tree)->root, 0, &printIntLine);
     return 1;
   } else {
     int user = rand_int(num_users, distrib, geo_param);    
+    printf("user: %d\n", user);
     void *user_node = findAndRemoveNode(multicast->users, user);
-    mult_rem(multicast, user_node);
+    int *remdata = (int *) mult_rem(multicast, user_node);
+    //pretty_traverse_tree(((struct LBBT *)multicast->tree)->root, 0, &printIntLine);
+    printf("rem: %d\n", *remdata);
+    free(remdata);
     return 2;
   }
 }
@@ -46,6 +70,9 @@ int main()
 {
   int n, k, lbbt_adds, lbbt_trunc, btree_adds, btree_deg, distrib;
   float add_wt, upd_wt, geo_param;
+
+  srand(time(0));
+  
   printf("Please enter your sequence options: ([init group size] [num ops] [add weight] [update weight])\n");
   scanf("%d", &n);
   scanf("%d", &k);
@@ -85,28 +112,28 @@ int main()
   struct List *users = malloc(sizeof(struct List));
   if (users == NULL) {
     perror("malloc returned NULL");
-    return NULL;
+    return -1;
   }
   initList(users); // new users added to front
   
   void **ids = malloc(sizeof(void *) * n);
   if (ids == NULL) {
     perror("malloc returned NULL");
-    return NULL;
+    return -1;
   }
   
   int i;
   int *max_id = malloc(sizeof(int));
   if (max_id == NULL) {
     perror("malloc returned NULL");
-    return NULL;
+    return -1;
   }
   
   for (i = 0; i < n; i++) {
     int *data = malloc(sizeof(int));
     if (data == NULL) {
       perror("malloc returned NULL");
-      return NULL;
+      return -1;
     }
     *data = i;
     *(ids + i) = (void *) data;    
@@ -117,17 +144,19 @@ int main()
   int *counts = malloc(sizeof(int) * 1); //TODO: just counting encs for now
   if (counts == NULL) {
     perror("malloc returned NULL");
-    return NULL;
+    return -1;
   }
   *counts = 0;
   
   struct Multicast *lbbt_multicast = malloc(sizeof(struct Multicast));
   if (lbbt_multicast == NULL) {
     perror("malloc returned NULL");
-    return NULL;
+    return -1;
   }
   struct Multicast multicast = { 1, users, (void *) lbbt, counts, 0 }; //TODO: fix this
   *lbbt_multicast = multicast;
+
+  //pretty_traverse_tree(((struct LBBT *)lbbt_multicast->tree)->root, 0, &printIntLine);
 
   int ops[3] = { 0, 0, 0 };
   
@@ -136,4 +165,20 @@ int main()
   }
 
   printf("# adds: %d, # updates: %d, # rems %d\n", ops[0], ops[1], ops[2]);
+
+  destroy_tree(((struct LBBT *) lbbt_multicast->tree)->root);
+  removeAllNodes(((struct LBBT *) lbbt_multicast->tree)->blanks);
+  free(((struct LBBT *) lbbt_multicast->tree)->blanks);
+  free(lbbt_multicast->tree);
+
+  removeAllNodes(users);
+  free(users);
+
+  free(ids);
+  free(counts);
+  free(lbbt_multicast);
+
+  free(max_id);
+
+  return 0;
 }
