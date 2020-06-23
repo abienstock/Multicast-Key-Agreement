e393a03c41311200515685705608db3b8b18c774
Alex Bienstock <afb383@cs.nyu.edu>
2020-06-23 17:41:45 -0400

    bug fixes

--- driver/test_driver.c
+++ driver/test_driver.c
@@ -102,8 +102,10 @@ int next_op(struct Multicast *multicast, struct List *users, float add_wt, float
     struct MultAddRet add_ret = mult_add(multicast, *max_id, NULL, NULL);
     //pretty_traverse_tree(((struct LBBT *)multicast->tree)->root, 0, &printNode);
     //pretty_traverse_skeleton(add_ret.skeleton, 0, &printSkeleton);
-    struct User *user = init_user(*max_id);
-    addAfter(users, users->tail, (void *) user);
+    if (multicast->crypto) {
+      struct User *user = init_user(*max_id);
+      addAfter(users, users->tail, (void *) user);
+    }
     id = *max_id;
     skeleton = add_ret.skeleton;
     oob_seeds = add_ret.oob_seeds;
@@ -124,8 +126,10 @@ int next_op(struct Multicast *multicast, struct List *users, float add_wt, float
     struct RemRet rem_ret = mult_rem(multicast, user_num, NULL, NULL);
     //pretty_traverse_tree(((struct LBBT *)multicast->tree)->root, 0, &printNode);
     //pretty_traverse_skeleton(rem_ret.skeleton, 0, &printSkeleton);
-    struct User *user = (struct User *) findAndRemoveNode(users, user_num);
-    destroy_user(user);
+    if (multicast->crypto) {
+      struct User *user = (struct User *) findAndRemoveNode(users, user_num);
+      destroy_user(user);
+    }
     id = ((struct NodeData *) rem_ret.data)->id;
     printf("rem: %d\n", id);      
     skeleton = rem_ret.skeleton;
@@ -146,7 +150,7 @@ int main(int argc, char *argv[]) {
 
   srand(time(0));
 
-  if (argc != 10 && argc != 11) {
+  if (argc != 11 && argc != 12) {
     printf("Usage: [testing] [init group size] [num ops] [add weight] [update weight] [LBBT addition strategy] [LBBT truncation strategy] [B-tree addition strategy] [B-tree degree] [operation distribution] [geometric distribution parameter] \n\nParameter Description:\n[crypto]: whether or not to test actual key agreement -- 0 = no, 1 = yes\n[init group size]: number of users in group at initialization\n[num ops]: number of group operations\n[add weight]: probability that a given operation will be an add (in [0,1])\n[update weight]: probability that a given operation will be an update (in [0,1]). Note: add weight + update weight must be in [0,1], remove weight = 1 - add weight - remove weight (probability that a given operation will be a remove)\n[LBBT addition strategy]: The strategy used to add users in an LBBT tree -- 0 = greedy, 1 = random, 2 = append, 3 = compare all\n[LBBT truncation strategy]: The strategy used to truncate the LBBT tree after removals -- 0 = truncate, 1 = keep, 2 = compare all\n[B-tree addition strategy]: The strategy used to add users in B trees -- 0 = greedy, 1 = random, 2 = compare both\n[B-tree degree]: Degree of the B-tree -- 3 = 3, 4 = 4, 0 = compare both\n[operation distribution]: The distribution for choosing which users to perform operations (and for the user to be removed, if the operation is a removal) -- 0 = uniform, 1 = geometric\n[geometric distribution parameter]: The p parameter of the geometric distribution, if chosen\n");
     exit(0);
   }
